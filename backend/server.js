a943cddb79546895890d125c2c3d35aec1b98f2e
Tony Wahula <mawahula4@gmail.com>
2019-04-13 20:26:44 -0600

    add dependency in root to run client and server concurrently

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+// const bodyParser  = require('body-parser');
+const cors = require("cors");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  // res.send("Hello World!");
+  res.send({ express: "Hello From Express" });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,9 +5,22 @@ import AddNote from "./AddNote";
 import EditNote from "./EditNote";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+  callAPI() {
+    fetch("/api")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+  componentDidMount() {
+    this.callAPI();
+  }
   render() {
     return (
       <div className="App">
+        <p className="App-intro">{this.state.apiResponse}</p>
         <Switch>
           <Route exact path="/" render={() => <Home />} />
           <Route path="/AddNote" render={() => <AddNote />} />
